8c9b33356025d33e6809e76c97b37c7cd572c16d
praveen-soni-by <praveen.soni@blueyonder.com>
2021-09-19 14:42:39 +0530

    added custom scalar

--- src/main/java/com/syscho/graphql/book/BookVO.java
+++ src/main/java/com/syscho/graphql/book/BookVO.java
@@ -3,6 +3,7 @@ package com.syscho.graphql.book;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Data
 public class BookVO {
@@ -11,4 +12,5 @@ public class BookVO {
     private String genre;
     private String publisher;
     private LocalDate publishDate;
+    private LocalDateTime createdTime;
 }
