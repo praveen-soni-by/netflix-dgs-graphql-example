280e7ebb4e31062030880b7ef30e89d0620780d2
praveen-soni-by <praveen.soni@blueyonder.com>
2021-09-19 15:41:57 +0530

    added integration test case

--- src/main/java/com/syscho/graphql/NetflixDsgGraphqlApplication.java
+++ src/main/java/com/syscho/graphql/NetflixDsgGraphqlApplication.java
@@ -2,12 +2,18 @@ package com.syscho.graphql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = {
+        "com.syscho.graphql.company.repository",
+        "com.syscho.graphql.book"
+})
+
 public class NetflixDsgGraphqlApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NetflixDsgGraphqlApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NetflixDsgGraphqlApplication.class, args);
+    }
 
 }
