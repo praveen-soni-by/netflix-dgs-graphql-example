7542c3aa88ae9c20ee1bb2d8e1d9559501aa746b
praveen-soni-by <praveen.soni@blueyonder.com>
2021-09-20 16:24:18 +0530

    added boolean,Enum data type

---
+++ src/main/java/com/syscho/graphql/instrumentation/ExampleTracingInstrumentation.java
@@ -0,0 +1,82 @@
+package com.syscho.graphql.instrumentation;
+
+import graphql.ExecutionResult;
+import graphql.execution.instrumentation.InstrumentationContext;
+import graphql.execution.instrumentation.InstrumentationState;
+import graphql.execution.instrumentation.SimpleInstrumentation;
+import graphql.execution.instrumentation.parameters.InstrumentationExecutionParameters;
+import graphql.execution.instrumentation.parameters.InstrumentationFieldFetchParameters;
+import graphql.schema.DataFetcher;
+import graphql.schema.GraphQLNonNull;
+import graphql.schema.GraphQLObjectType;
+import graphql.schema.GraphQLOutputType;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CompletableFuture;
+
+@Component
+@Slf4j
+public class ExampleTracingInstrumentation extends SimpleInstrumentation {
+
+    @Override
+    public InstrumentationState createState() {
+        return new TracingState();
+    }
+
+    @Override
+    public InstrumentationContext<ExecutionResult> beginExecution(InstrumentationExecutionParameters parameters) {
+        TracingState tracingState = parameters.getInstrumentationState();
+        tracingState.startTime = System.currentTimeMillis();
+        return super.beginExecution(parameters);
+    }
+
+    @Override
+    public DataFetcher<?> instrumentDataFetcher(DataFetcher<?> dataFetcher, InstrumentationFieldFetchParameters parameters) {
+        // We only care about user code
+        if (parameters.isTrivialDataFetcher()) {
+            return dataFetcher;
+        }
+
+        return environment -> {
+            long startTime = System.currentTimeMillis();
+            Object result = dataFetcher.get(environment);
+            if (result instanceof CompletableFuture) {
+                ((CompletableFuture<?>) result).whenComplete((r, ex) -> {
+                    long totalTime = System.currentTimeMillis() - startTime;
+                    log.info("Async datafetcher {} took {}ms", findDataFetcherTag(parameters), totalTime);
+                });
+            } else {
+                long totalTime = System.currentTimeMillis() - startTime;
+                log.info("Datafetcher {} took {}ms", findDataFetcherTag(parameters), totalTime);
+            }
+
+            return result;
+        };
+    }
+
+    @Override
+    public CompletableFuture<ExecutionResult> instrumentExecutionResult(ExecutionResult executionResult, InstrumentationExecutionParameters parameters) {
+        TracingState tracingState = parameters.getInstrumentationState();
+        long totalTime = System.currentTimeMillis() - tracingState.startTime;
+        log.info("Total execution time: {}ms", totalTime);
+
+        return super.instrumentExecutionResult(executionResult, parameters);
+    }
+
+    private String findDataFetcherTag(InstrumentationFieldFetchParameters parameters) {
+        GraphQLOutputType type = parameters.getExecutionStepInfo().getParent().getType();
+        GraphQLObjectType parent;
+        if (type instanceof GraphQLNonNull) {
+            parent = (GraphQLObjectType) ((GraphQLNonNull) type).getWrappedType();
+        } else {
+            parent = (GraphQLObjectType) type;
+        }
+
+        return parent.getName() + "." + parameters.getExecutionStepInfo().getPath().getSegmentName();
+    }
+
+    static class TracingState implements InstrumentationState {
+        long startTime;
+    }
+}
